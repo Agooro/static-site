01dbead14d5b73e13b76d6b32c11badbf281730c
Agooro <agooro.art@gmail.com>
2025-08-09 13:26:02 +0530

    Deploy site to docs for GitHub Pages

--- src/gencontent.py
+++ src/gencontent.py
@@ -3,29 +3,31 @@ from pathlib import Path
 from markdown_blocks import markdown_to_html_node
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
         if os.path.isfile(from_path):
             dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
         else:
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f" * {from_path} {template_path} -> {dest_path}")
-    from_file = open(from_path, "r")
-    markdown_content = from_file.read()
-    from_file.close()
+    with open(from_path, "r") as from_file:
+        markdown_content = from_file.read()
 
-    template_file = open(template_path, "r")
-    template = template_file.read()
-    template_file.close()
+    with open(template_path, "r") as template_file:
+        template = template_file.read()
 
     node = markdown_to_html_node(markdown_content)
     html = node.to_html()
+
+    # Replace basepath in links and images
+    html = html.replace('href="/', f'href="{basepath}')
+    html = html.replace('src="/', f'src="{basepath}')
 
     title = extract_title(markdown_content)
     template = template.replace("{{ Title }}", title)
@@ -34,8 +36,8 @@ def generate_page(from_path, template_path, dest_path):
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
         os.makedirs(dest_dir_path, exist_ok=True)
-    to_file = open(dest_path, "w")
-    to_file.write(template)
+    with open(dest_path, "w") as to_file:
+        to_file.write(template)
 
 
 def extract_title(md):
